5ac02a3d3dec0e04313360212cde150adeca2ca5
Jon Baclig <jon@baclig.com>
2018-05-04 13:17:53 -0700

    add navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-import 'bootstrap';
+document.addEventListener('DOMContentLoaded', () => {
+
+    let navbar = document.querySelector('.navbar');
+    document.addEventListener('scroll', () => {
+        if(window.scrollY === 0) {
+            navbar.classList.add('transparent');
+        }
+        else {
+            if(navbar.classList.contains('transparent')) {
+                navbar.classList.remove('transparent');
+            }
+        }
+    });
+    
+
+});

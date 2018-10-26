da38654f57477568c3c2f5286d559d463c721b68
Jon Baclig <jon@baclig.com>
2018-10-26 16:29:23 -0700

    add RSVP link

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,5 +1,5 @@
 Handlebars.registerHelper('navigation', function(context, options) {
-    var ret = '<ul class="navbar-nav ml-auto mt-2 mt-lg-0">';
+    var ret = '<ul class="navbar-nav ml-auto mt-2 mt-lg-0"><li class="nav-item rsvp"><a class="nav-link" href="https://rsvp.jkbaclig.com" target="_blank">RSVP</a></li>';
   
     for(var i=0, j=context.length; i<j; i++) {
         ret = ret + '<li class="nav-item">' + options.fn(context[i]) + '</li>';
--- src/js/templates.js
+++ src/js/templates.js
@@ -28,7 +28,7 @@ this["MyApp"]["templates"]["details"] = Handlebars.template({"compiler":[7,">= 4
 this["MyApp"]["templates"]["nav"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "            <a class=\"nav-link\" href=\""
+  return "        <a class=\"nav-link\" href=\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
     + "\">"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
@@ -36,11 +36,11 @@ this["MyApp"]["templates"]["nav"] = Handlebars.template({"1":function(container,
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing;
 
-  return "<nav class=\"navbar navbar-expand-lg navbar-custom transparent navbar-light fixed-top\">\n    <a class=\"navbar-brand font-fancy\" href=\"index.html\">"
+  return "<nav class=\"navbar navbar-expand-lg navbar-custom transparent navbar-light fixed-top py-0\">\n    <a class=\"navbar-brand font-fancy\" href=\"index.html\">"
     + container.escapeExpression(((helper = (helper = helpers.brand || (depth0 != null ? depth0.brand : depth0)) != null ? helper : alias2),(typeof helper === "function" ? helper.call(alias1,{"name":"brand","hash":{},"data":data}) : helper)))
-    + "</a>\n    <button class=\"navbar-toggler collapsed\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarTogglerDemo02\" aria-controls=\"navbarTogglerDemo02\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n        <i class=\"fas fa-ellipsis-h\"></i>\n    </button>\n              \n    <div class=\"collapse navbar-collapse\" id=\"navbarTogglerDemo02\">\n"
+    + "</a>\n    <button class=\"navbar-toggler collapsed\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarTogglerDemo02\" aria-controls=\"navbarTogglerDemo02\"\n        aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n        <i class=\"fas fa-ellipsis-h\"></i>\n    </button>\n\n    <div class=\"collapse navbar-collapse\" id=\"navbarTogglerDemo02\">\n"
     + ((stack1 = (helpers.navigation || (depth0 && depth0.navigation) || alias2).call(alias1,(depth0 != null ? depth0.nav : depth0),{"name":"navigation","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "    </div>\n</nav>\n";
+    + "    </div>\n</nav>";
 },"useData":true});
 this["MyApp"]["templates"]["pageHeader"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
